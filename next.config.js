76b7447d5c33b84bf63e3ea15f084a74682a0b43
zhonglinxin <fjgysai@qq.com>
2025-06-03 16:32:37 +0800

    更新依赖

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
+  // appDir is now stable and enabled by default in Next.js 14
+  // No experimental configuration needed
 }
 
 module.exports = nextConfig 
